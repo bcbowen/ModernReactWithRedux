4d653b54cde93d2874610d55fb52da00d8d05beb
Ben Bowen <bbowen@bhgpdi.net>
2021-03-13 10:39:30 -0500

    Add buttons

--- 140_widgets/src/components/Search.js
+++ 140_widgets/src/components/Search.js
@@ -26,6 +26,14 @@ const Search = () => {
   const renderedResults = results.map((result) => {
     return (
       <div key={result.pageId} className="item">
+        <div className="right floated content">
+          <a
+            className="ui button"
+            href={`https://en.wikipedia.org?curid=${result.pageid}`}
+          >
+            Go
+          </a>
+        </div>
         <div className="content">
           <div className="header">{result.title}</div>
           <div>
